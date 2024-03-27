aa4f0a135a1713a481ac25a689d647fb546de4d0
River7784 <119822951+River7784@users.noreply.github.com>
2024-03-27 19:52:16 +0900

    Add files via upload

---
+++ c_06/c_06/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  c_06
+//
+//  Created by 유가람 on 3/27/24.
+//
+
+#include <stdio.h>
+void main(){
+    char ch;
+    int in;
+    ch = in = 'A';
+    printf("ch = %d \n", ch);
+    printf("in = %d \n", in);
+    printf("--------- \n");
+    ch = in = 100;
+    printf("ch = %c \n", ch);
+    printf("in = %c \n", in);
+}
