89be781205793299c4262939bb22af01e1cbebad
River7784 <119822951+River7784@users.noreply.github.com>
2024-03-28 19:42:05 +0900

    Add files via upload

---
+++ c_12/c_12/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  c_12
+//
+//  Created by 유가람 on 3/28/24.
+//
+
+/*선행처리기*/
+#include <stdio.h>
+#define ADDR "서울특별시 종로구"
+#define TEL "02-xxxx-1234"
+
+void main(){
+    printf("주소 : %s \n", ADDR);
+    printf("전화번호 : %s \n", TEL);
+}
