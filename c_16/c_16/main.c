0af3473a1fab0108d1d9c731bde38c0634711109
River7784 <119822951+River7784@users.noreply.github.com>
2024-03-31 11:52:45 +0900

    Add files via upload

---
+++ c_16/c_16/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_16
+//
+//  Created by 유가람 on 3/31/24.
+//
+
+#include "Header_01.h"
+
+void main(){
+    int add_result, sub_result;
+    printf("10과 5를 더하면 %d이다. \n", ADD(10,5));
+    printf("10에서 5를 빼면 %d이다. \n", SUB(10, 5));
+}
