0c5f8145a6ccbaeda6d2d5da08e1dd4a8df72961
River7784 <119822951+River7784@users.noreply.github.com>
2024-04-05 19:35:38 +0900

    Add files via upload

---
+++ c_23/c_23/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  c_23
+//
+//  Created by 유가람 on 4/5/24.
+//
+
+//산술 연산자
+#include <stdio.h>
+void main(){
+    int x, y;
+    x=10;
+    y=3;
+    printf("x + y = %d \n", x + y);
+    printf("x / y = %d \n", x / y);
+    printf("x %% y = %d \n", x % y);
+    printf("y %% x = %d \n", y % x);
+}
