31ae3154c47135b67d31f73364bf2b9795307ac8
River7784 <119822951+River7784@users.noreply.github.com>
2024-04-10 12:03:31 +0900

    Add files via upload

---
+++ c_27/c_27/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  c_27
+//
+//  Created by 유가람 on 4/9/24.
+//
+
+#include <stdio.h>
+
+void main(){
+    float a = 3.14;
+    printf("int형의 크기 = %d바이트 \n", sizeof(int));
+    //int형의 자료 크기를 출력
+    printf("float형 변수 a의 크기 = %d바이트 \n", sizeof(a));
+    //float형인 a의 크기를 출력
+}
