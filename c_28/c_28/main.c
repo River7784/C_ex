7bb17ea465d520142c298746edaa4ffb57d29120
River7784 <119822951+River7784@users.noreply.github.com>
2024-04-11 19:49:17 +0900

    Add files via upload

---
+++ c_28/c_28/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  c_28
+//
+//  Created by 유가람 on 4/11/24.
+//
+
+#include <stdio.h>
+void main(){
+    int a = 3, b = 4;
+    double c, d;
+    c = a / b;
+    printf("나눗셈 결과 : %f \n", c);
+    d = (double)a / b;
+    printf("나눗셈 결과 : %f \n", d);
+}
