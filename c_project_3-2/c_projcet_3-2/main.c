a991f8fdbdf8f656591e29fd89fe64e9eb1add87
River7784 <119822951+River7784@users.noreply.github.com>
2024-06-30 21:20:27 +0900

    Add files via upload

---
+++ c_project_3-2/c_projcet_3-2/main.c
@@ -0,0 +1,24 @@
+//<과제 2> [예제 3-2]
+
+#include <stdio.h>
+#pragma  warning(disable:4996);
+void main(){
+    int in_money, coin_500, coin_100, coin_50, coin_10;
+    printf(" 동전으로 교환할 금액을 입력하세요 : ");
+    scanf("%d", &in_money);     //금액을 입력받아서 in_money 변수에 저장
+    
+    coin_500 = in_money / 500;
+    in_money = in_money % 500;
+    coin_100 = in_money / 100;
+    in_money = in_money % 100;
+    coin_50 = in_money / 50;
+    in_money = in_money % 50;
+    coin_10 = in_money / 10;
+    in_money = in_money % 10;
+    
+    printf("\n 오백원 =>  %d개 \n", coin_500);
+    printf("\n 백원 =>  %d개 \n", coin_100);
+    printf("\n 오십원 =>  %d개 \n", coin_50);
+    printf("\n 십원 =>  %d개 \n", coin_10);
+    printf("\n 바꾸지 못한 잔돈 =>  %d개 \n", in_money);
+}
