d190c5f5eaecab83562258c171057308c8837faa
River7784 <119822951+River7784@users.noreply.github.com>
2024-07-06 21:13:34 +0900

    Add files via upload

---
+++ c_80/c_80/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  c_80
+//
+//  Created by 유가람 on 7/6/24.
+//
+
+#include <stdio.h>
+#include <alloca.h>
+
+void main(){
+    char *str[5];
+    int i;
+    
+    for(i=0; i<5; i++){
+        if ((str[i]=(char*)malloc(128)==NULL){
+            printf("메모리 할당 실패 \n");
+            exit(0);
+        }
+            printf("메모리 할당 성공, 문자열[%d]을 입력하세요 \n", i+1);
+            gets(str[i]);
+    }
+        for(i=0; i<5; i++){
+            free(str[i]);
+        }
+}
